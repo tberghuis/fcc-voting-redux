87cec66f315f5234f46c18271a2eb2f721a0b5b6
Tom Berghuis <thomas.berghuis@gmail.com>
2017-05-05 16:57:07 +1000

    server

--- server/app.js
+++ server/app.js
@@ -12,31 +12,34 @@ var fs = require('fs'),
   errorhandler = require('errorhandler'),
   mongoose = require('mongoose');
 
-// npm i -S fs http path methods express body-parser express-session cors passport errorhandler mongoose 
-
 var isProduction = process.env.NODE_ENV === 'production';
 
 var app = express();
 
 app.use(cors());
 
-app.use(require('morgan')('dev'));
-app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(require('morgan')('dev'));
+// app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 //app.use(require('method-override')());
 //app.use(express.static(__dirname + '/public'));
 
-//app.use(session({ secret: 'conduit', cookie: { maxAge: 60000 }, resave: false, saveUninitialized: false  }));
+//console.log('SECRET',process.env.SESSION_SECRET);
+// study what the options do....
+
+// this session shit is not even relevant...
+// ok, post a bug report asking why it exists?
+//app.use(session({ secret: process.env.SESSION_SECRET, cookie: { maxAge: 60000 }, resave: false, saveUninitialized: false  }));
 
 if (!isProduction) {
   app.use(errorhandler());
 }
 
 mongoose.connect(process.env.MONGODB_URI);
-if (!isProduction) {
-  // mongoose.set('debug', true);
-}
+// if (!isProduction) {
+//   // mongoose.set('debug', true);
+// }
 
 
 require('./models/User');
--- server/models/User.js
+++ server/models/User.js
@@ -1,43 +1,46 @@
 var mongoose = require('mongoose');
-// var uniqueValidator = require('mongoose-unique-validator');
-// var crypto = require('crypto');
-// var jwt = require('jsonwebtoken');
-// var secret = require('../config').secret;
+var uniqueValidator = require('mongoose-unique-validator');
+var crypto = require('crypto');
+var jwt = require('jsonwebtoken');
+//var secret = require('../config').secret;
 
 var UserSchema = new mongoose.Schema({
   username: {type: String, lowercase: true, unique: true, required: [true, "can't be blank"], match: [/^[a-zA-Z0-9]+$/, 'is invalid'], index: true},
   email: {type: String, lowercase: true, unique: true, required: [true, "can't be blank"], match: [/\S+@\S+\.\S+/, 'is invalid'], index: true},
+  hash: String,
+  salt: String
 }, {timestamps: true});
 
-// UserSchema.plugin(uniqueValidator, {message: 'is already taken.'});
+UserSchema.plugin(uniqueValidator, {message: 'is already taken.'});
 
-// UserSchema.methods.generateJWT = function() {
-//   var today = new Date();
-//   var exp = new Date(today);
-//   exp.setDate(today.getDate() + 60);
+UserSchema.methods.validPassword = function(password) {
+  var hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
+  return this.hash === hash;
+};
 
-//   return jwt.sign({
-//     id: this._id,
-//     username: this.username,
-//     exp: parseInt(exp.getTime() / 1000),
-//   }, secret);
-// };
+UserSchema.methods.setPassword = function(password){
+  this.salt = crypto.randomBytes(16).toString('hex');
+  this.hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
+};
 
-// UserSchema.methods.toAuthJSON = function(){
-//   return {
-//     username: this.username,
-//     email: this.email,
-//     token: this.generateJWT()
-//   };
-// };
+UserSchema.methods.generateJWT = function() {
+  var today = new Date();
+  var exp = new Date(today);
+  exp.setDate(today.getDate() + 60);
 
-// UserSchema.methods.toProfileJSONFor = function(user){
-//   return {
-//     username: this.username,
-//     bio: this.bio,
-//     image: this.image || 'https://static.productionready.io/images/smiley-cyrus.jpg',
-//     following: user ? user.isFollowing(this._id) : false
-//   };
-// };
+  return jwt.sign({
+    id: this._id,
+    username: this.username,
+    exp: parseInt(exp.getTime() / 1000),
+  }, process.env.JWT_SECRET);
+};
 
-module.exports = mongoose.model('User', UserSchema);
+UserSchema.methods.toAuthJSON = function(){
+  return {
+    username: this.username,
+    email: this.email,
+    token: this.generateJWT()
+  };
+};
+
+mongoose.model('User', UserSchema);
